2978d9fdd0c500a87827b1bf5cd5c3278e780474
Falileeva Maria <raduga.mf@gmail.com>
2022-09-16 11:39:29 +0800

    first commit

---
+++ submission.py
@@ -0,0 +1,121 @@
+from typing import Callable, List, Set
+
+import shell
+import util
+
+
+############################################################
+# Problem 1b: Solve the segmentation problem under a unigram model
+
+class SegmentationProblem(util.SearchProblem):
+
+    def __init__(self, query: str, unigramCost: Callable[[str], float]):
+        self.query = query
+        self.unigramCost = unigramCost
+
+    def startState(self):
+        return self.query
+
+    def isEnd(self, state) -> bool:
+        return len(state) == 0
+
+    def succAndCost(self, state):
+        result = []
+        print(state)
+        if len(state) > 0:
+            for i in range(len(state), 0, -1):
+                action = state[:i]
+                remainder = state[len(action):]  # оставшийся текст
+                result.append((
+                    action,
+                    remainder,
+                    self.unigramCost(action)
+                ))
+        print(result)
+
+        return result
+
+
+def segmentWords(query: str, unigramCost: Callable[[str], float]) -> str:
+    if len(query) == 0:
+        return ''
+
+    ucs = util.UniformCostSearch(verbose=1)
+    ucs.solve(SegmentationProblem(query, unigramCost))
+
+    return ' '.join(ucs.actions)
+
+
+############################################################
+# Problem 2b: Solve the vowel insertion problem under a bigram cost
+
+class VowelInsertionProblem(util.SearchProblem):
+    def __init__(self, queryWords: List[str], bigramCost: Callable[[str, str], float],
+                 possibleFills: Callable[[str], Set[str]]):
+        self.queryWords = queryWords
+        self.bigramCost = bigramCost
+        self.possibleFills = possibleFills
+
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+    def isEnd(self, state) -> bool:
+        # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+    def succAndCost(self, state):
+        # BEGIN_YOUR_CODE (our solution is 8 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+
+def insertVowels(queryWords: List[str], bigramCost: Callable[[str, str], float],
+                 possibleFills: Callable[[str], Set[str]]) -> str:
+    # BEGIN_YOUR_CODE (our solution is 3 lines of code, but don't worry if you deviate from this)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
+
+
+############################################################
+# Problem 3b: Solve the joint segmentation-and-insertion problem
+
+class JointSegmentationInsertionProblem(util.SearchProblem):
+    def __init__(self, query: str, bigramCost: Callable[[str, str], float],
+                 possibleFills: Callable[[str], Set[str]]):
+        self.query = query
+        self.bigramCost = bigramCost
+        self.possibleFills = possibleFills
+
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+    def isEnd(self, state) -> bool:
+        # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+    def succAndCost(self, state):
+        # BEGIN_YOUR_CODE (our solution is 14 lines of code, but don't worry if you deviate from this)
+        raise Exception("Not implemented yet")
+        # END_YOUR_CODE
+
+
+def segmentAndInsert(query: str, bigramCost: Callable[[str, str], float],
+                     possibleFills: Callable[[str], Set[str]]) -> str:
+    if len(query) == 0:
+        return ''
+
+    # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
+
+
+############################################################
+
+if __name__ == '__main__':
+    shell.main()
